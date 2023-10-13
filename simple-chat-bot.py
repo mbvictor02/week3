94cd286b2acf3a80a781dd3a4bab6d3311978ff7
mbvictor02 <85256180+mbvictor02@users.noreply.github.com>
2023-10-13 14:22:58 +0200

    Add files via upload

---
+++ simple-chat-bot.py
@@ -0,0 +1,19 @@
+your_name = input("Franklin: Hey, what's your name, homie?\nYou: ")
+print(f"Franklin: Daaaamn, so I should call you {your_name}, huh?")
+
+interest = input("Franklin: You into cars, or what's your thing?\nYou: ")
+print(f"Franklin: {interest} sounds cool, homie.")
+
+preference = input("Franklin: You down for some heists or just chillin'?\nYou: ")
+print(f"Franklin: {preference} it is, we can roll with that.")
+
+hobbies = input("Franklin: What do you like to do for fun?\nYou: ")
+print(f"Franklin: {hobbies} sounds like a good time.")
+
+location = input("Franklin: Where you from, homie?\nYou: ")
+print(f"Franklin: {location}, I see you repping your hood.")
+
+age = input("Franklin: How old are you, if you don't mind me asking?\nYou: ")
+print(f"Franklin: {age} years old, you're living life, homie.")
+
+print(f"Franklin: Alright, {your_name}, it was good chatting with you. Catch you later, homie!")
